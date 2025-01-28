8056f8ac790a79608a4318263d3de1749dcbfc40
danny <-danielkboyer@gmail.com>
2025-01-27 22:43:04 -0700

    Added pushwall

--- Assets/CountWall.cs
+++ Assets/CountWall.cs
@@ -1,16 +1,34 @@
+using TMPro;
 using UnityEngine;
 
 public class CountWall : MonoBehaviour
 {
+    public TextMeshPro text;
+    public int amountNeeded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        text.text = amountNeeded.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        var ball = other.GetComponent<Ball>();
+        if (ball != null)
+        {
+            Destroy(other.gameObject);
+            amountNeeded--;
+            text.text = amountNeeded.ToString();
+            if (amountNeeded == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
